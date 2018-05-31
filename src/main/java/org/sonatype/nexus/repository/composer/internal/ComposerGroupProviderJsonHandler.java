8b43ec5dce6ea0197dc7fc5344ef482be3e8e0cc
Frederick John Milens III <fmilens@sonatype.com>
2018-05-31 12:02:15 -0400

    Combine operations into single stream traversal (different location)

--- src/main/java/org/sonatype/nexus/repository/composer/internal/ComposerGroupProviderJsonHandler.java
+++ src/main/java/org/sonatype/nexus/repository/composer/internal/ComposerGroupProviderJsonHandler.java
@@ -57,13 +57,13 @@ public class ComposerGroupProviderJsonHandler
     Repository repository = context.getRepository();
     GroupFacet groupFacet = repository.facet(GroupFacet.class);
     Map<Repository, Response> responses = getAll(context, groupFacet.members(), dispatched);
-    List<Response> successfulResponses = responses.values().stream()
+    List<Payload> payloads = responses.values().stream()
         .filter(response -> response.getStatus().getCode() == HttpStatus.OK && response.getPayload() != null)
+        .map(Response::getPayload)
         .collect(Collectors.toList());
-    if (successfulResponses.isEmpty()) {
+    if (payloads.isEmpty()) {
       return notFoundResponse(context);
     }
-    List<Payload> payloads = successfulResponses.stream().map(Response::getPayload).collect(Collectors.toList());
     return HttpResponses.ok(composerJsonProcessor.mergeProviderJson(repository, payloads, DateTime.now()));
   }
 }
