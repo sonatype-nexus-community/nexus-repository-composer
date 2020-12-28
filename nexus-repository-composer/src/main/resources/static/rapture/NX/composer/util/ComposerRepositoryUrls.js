a6629f049515661210c4a864c26bcf7643de5e90
Mykyta Shevelov <mshevelov@sonatype.com>
2020-12-28 11:51:17 +0200

    NEXUS-25906 Extra slash in the URI for asset in community formats (#84)

--- nexus-repository-composer/src/main/resources/static/rapture/NX/composer/util/ComposerRepositoryUrls.js
+++ nexus-repository-composer/src/main/resources/static/rapture/NX/composer/util/ComposerRepositoryUrls.js
@@ -19,8 +19,10 @@ Ext.define('NX.composer.util.ComposerRepositoryUrls', {
     'NX.util.Url'
   ]
 }, function(self) {
-  NX.coreui.util.RepositoryUrls.addRepositoryUrlStrategy('composer', function(assetModel) {
+  NX.coreui.util.RepositoryUrls.addRepositoryUrlStrategy('composer', function(me, assetModel) {
     var repositoryName = assetModel.get('repositoryName'), assetName = assetModel.get('name');
-    return NX.util.Url.asLink(NX.util.Url.baseUrl + '/repository/' + repositoryName + '/' + assetName, assetName);
+    return NX.util.Url.asLink(
+        NX.util.Url.baseUrl + '/repository/' + encodeURIComponent(repositoryName) + '/' + encodeURI(assetName),
+        assetName);
   });
 });
