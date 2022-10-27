61fb9854f1977fc9b43a33d4d65f72ea63bc7b77
Locco123456 <locco1_2_3@outlook.de>
2022-10-27 11:11:08 +0200

    Use mergeProviderJson Method, if package structure is wrong

--- nexus-repository-composer/src/main/java/org/sonatype/nexus/repository/composer/internal/ComposerJsonProcessor.java
+++ nexus-repository-composer/src/main/java/org/sonatype/nexus/repository/composer/internal/ComposerJsonProcessor.java
@@ -427,7 +427,12 @@ public class ComposerJsonProcessor
         Map<String, Object> packagesMap = (Map<String, Object>) json.get(PACKAGES_KEY);
         for (String packageName : packagesMap.keySet()) {
 
-          List<Map<String, Object>> packageVersions = (List<Map<String, Object>>) packagesMap.get(packageName);
+          List<Map<String, Object>> packageVersions;
+          try{
+            packageVersions = (List<Map<String, Object>>) packagesMap.get(packageName);
+          }catch (Exception e){
+            return mergeProviderJson(repository,payloads,now);
+          }
           for (Map<String, Object> versionInfo : packageVersions) {
             String packageVersion = (String) versionInfo.get(VERSION_KEY);
 
