b2064553c6cf43cfb7a632c8fba388446fb381b1
Niklas Grebe <thyphoon@5on5.org>
2022-11-08 00:29:06 +0100

    Implement cleanup policy (#99)
    * Add cleanup config
    
    * Fix imports, add cleanup to hosted repo as well
    
    Co-authored-by: Dan Rollo <danrollo@gmail.com>

--- nexus-repository-composer/src/main/resources/static/rapture/NX/composer/view/repository/recipe/ComposerProxy.js
+++ nexus-repository-composer/src/main/resources/static/rapture/NX/composer/view/repository/recipe/ComposerProxy.js
@@ -22,7 +22,8 @@ Ext.define('NX.composer.view.repository.recipe.ComposerProxy', {
     'NX.coreui.view.repository.facet.ProxyFacet',
     'NX.coreui.view.repository.facet.StorageFacet',
     'NX.coreui.view.repository.facet.HttpClientFacet',
-    'NX.coreui.view.repository.facet.NegativeCacheFacet'
+    'NX.coreui.view.repository.facet.NegativeCacheFacet',
+    'NX.coreui.view.repository.facet.CleanupPolicyFacet'
   ],
 
   /**
@@ -35,7 +36,8 @@ Ext.define('NX.composer.view.repository.recipe.ComposerProxy', {
       {xtype: 'nx-coreui-repository-proxy-facet'},
       {xtype: 'nx-coreui-repository-storage-facet'},
       {xtype: 'nx-coreui-repository-negativecache-facet'},
-      {xtype: 'nx-coreui-repository-httpclient-facet'}
+      {xtype: 'nx-coreui-repository-httpclient-facet'},
+      {xtype: 'nx-coreui-repository-cleanup-policy-facet'}
     ];
 
     me.callParent();
